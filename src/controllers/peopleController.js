7fd4d4c76dcf4e660cacda89e5abe98f6daee05f
krismac12 <kristanmacaraeg@gmail.com>
2023-05-23 14:08:10 +1000

    added roles and characters

--- src/controllers/peopleController.js
+++ src/controllers/peopleController.js
@@ -1,4 +1,8 @@
 const peopleModel = require('../models/peopleModel');
+const principalModel = require('../models/principalModel');
+const movieModel = require('../models/movieModel');
+
+
 
 const getPersonById = async (req, res) => {
     try {
@@ -6,13 +10,31 @@ const getPersonById = async (req, res) => {
     
         // Fetch the person by ID from the database
         const data = await peopleModel.getPersonById(id);
-    
+
+        const movieIDs = data.knownForTitles.split(",");
+        const roles = [];
+
+        await Promise.all(movieIDs.map(async (movieID) => {
+            const principals = await principalModel.getPrincipalsByID(movieID);
+            const matchingPrincipal = principals.find(principal => principal.nconst === id);
+            if (matchingPrincipal) {
+                const movie = await movieModel.getMovieById(movieID);
+                const role = {
+                    movieName: movie.primaryTitle,
+                    movieId: movie.tconst,
+                    category: matchingPrincipal.category,
+                    characters: JSON.parse(matchingPrincipal.characters),
+                    imdbRating: parseFloat(movie.imdbRating)
+                };
+                roles.push(role);
+            }
+          }));
         if (data) {
             person = {
                 name:data.primaryName,
                 birthYear:data.birthYear || null,
                 deathYear:data.deathYear || null,
-                roles: 1
+                roles: roles
             }
             res.json(person);
         } else {
